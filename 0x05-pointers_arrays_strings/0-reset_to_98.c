8893a4a873173ea3d5f4f1875aa64a67b616c109
= <=>
2023-03-29 14:34:35 -0700

    recomiting

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
+
 /**
-* main - check the code 
-* Return: Always 0.
-*/
-int main(void)
-{int n;
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
+
