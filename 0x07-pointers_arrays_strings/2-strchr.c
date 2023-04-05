2beffba81287992e80314d6844a4b7390b1c7cb8
= <=>
2023-04-05 04:41:38 -0700

    a function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,13 +3,14 @@
 
 /**
  * _strchr - Entry point
- * @s: input 
+ * @s: input
  * @c: input
  * Return: Always 0 (Success)
  */
-char *_strchr (char *s, char c)
+char *_strchr(char *s, char c)
 {
 	int i;
+
 	for (i = 0; s[i] >= '\0' ; i++)
 	{
 		if (s[i] == c)
