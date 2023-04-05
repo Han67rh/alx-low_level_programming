2bca878fc2b6992c28daa9223c1c912d1ecd00e6
= <=>
2023-04-05 03:45:48 -0700

    a function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
- 
 /**
  * _memset - entry point
  * @s : pointed destination
@@ -10,7 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	for (i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
 }
